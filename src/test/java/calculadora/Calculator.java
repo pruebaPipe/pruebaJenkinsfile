29369a5ff4d151298a18d734c88df406b154e0be
pruebaPipe <57288030+pruebaPipe@users.noreply.github.com>
2019-11-07 07:08:21 -0500

    Update Calculator.java

--- src/test/java/calculadora/Calculator.java
+++ src/test/java/calculadora/Calculator.java
@@ -1,7 +1,7 @@
 package calculadora;
 
 public class Calculator {
-	double resultado = 0;
+	
 	public static Object sumar(int i, int j) {
 		
 		return i + j;
@@ -15,9 +15,9 @@ public class Calculator {
 	public static Object dividir(int i, int j) {
 		
 		if(j == 0) {
-			trhrow new Ilegalargumentexception ("El divisor no puede ser 0, imagina las implicaciones!")
+			trhrow new Ilegalargumentexception ("El divisor no puede ser 0, imagina las implicaciones!");
 		}
-		return resultado;
+		return i / j;
 	}
 
 	public static Object usToPe(int i) {
